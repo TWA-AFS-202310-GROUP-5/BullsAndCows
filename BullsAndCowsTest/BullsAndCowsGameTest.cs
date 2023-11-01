dd116091ce51ccda2ed5e40d8fc600ed763b2fad
Jinglin Wang <JWang317@slb.com>
2023-11-01 16:45:08 +0800

    test: add ut for guess, should return 4A0B when guess given guess number and secret are same

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -13,5 +13,14 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
         }
+
+        [Fact]
+        public void Should_return_4A0B_when_Guess_given_()
+        {
+            var secretGenerator = new SecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            Assert.NotNull(game);
+            Assert.True(game.CanContinue);
+        }
     }
 }
