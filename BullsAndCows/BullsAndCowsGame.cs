28d12cba6afcb7e00c3d925545b2baefeb135370
Jinglin Wang <JWang317@slb.com>
2023-11-01 16:56:15 +0800

    feat: Guess should return "4A0B" when given guess same as secret

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,25 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            if (guess == secret)
+            {
+                return "4A0B";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
