8d5ef9f62503329d088e3f25ef06c5164858f95a
Zhiping Shu <ZShu@slb.com>
2023-11-01 17:11:50 +0800

    feat: should return 2A0B when guess given position and digit partial right

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -16,12 +16,21 @@ namespace BullsAndCows
 
         public string Guess(string guess)
         {
+            int bulls = 0;
             if (guess.Equals(secret))
             {
                 return "4A0B";
             }
 
-            return null;
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (guess[i] == secret[i])
+                {
+                    bulls++;
+                }
+            }
+
+            return $"{bulls}A0B";
         }
     }
 }
