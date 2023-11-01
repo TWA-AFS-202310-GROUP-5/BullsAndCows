0b444eace8c0b2b7acfef140a2f2dd52c9534641
Jinglin Wang <JWang317@slb.com>
2023-11-01 16:47:00 +0800

    Revert "test: add ut for guess, should return 4A0B when guess given guess number and secret are same"
    This reverts commit 4e2f5031997c3bb485634de76e00630a2467d5ac.

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,5 +1,4 @@
 using BullsAndCows;
-using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -13,22 +12,6 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
-        }
-
-        [Fact]
-        public void Should_return_4A0B_when_Guess_given_guess_number_and_secret_are_same()
-        {
-            //given
-            string guessNumber = "1234";
-            string secret = "1234";
-            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
-            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
-
-            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
-            //when
-            string result = game.Guess(guessNumber);
-            //then
-            Assert.Equal("4A0B", result);
         }
     }
 }
