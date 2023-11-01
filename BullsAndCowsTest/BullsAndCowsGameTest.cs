4e2f5031997c3bb485634de76e00630a2467d5ac
Jinglin Wang <JWang317@slb.com>
2023-11-01 16:45:08 +0800

    test: add ut for guess, should return 4A0B when guess given guess number and secret are same

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,22 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_Guess_given_guess_number_and_secret_are_same()
+        {
+            //given
+            string guessNumber = "1234";
+            string secret = "1234";
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //when
+            string result = game.Guess(guessNumber);
+            //then
+            Assert.Equal("4A0B", result);
         }
     }
 }
