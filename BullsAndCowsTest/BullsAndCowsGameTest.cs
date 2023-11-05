7414a6f0b29d844b5bb2b466716015be0421c1fe
Zhiping Shu <ZShu@slb.com>
2023-11-05 10:32:44 +0800

    fix: remove the exception of SecretGenerator

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,15 +8,6 @@ namespace BullsAndCowsTest
     public class BullsAndCowsGameTest
     {
         [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }
-
-        [Fact]
         public void Should_return_4A0B_when_guess_given_guess_number_and_secret_are_same()
         {
             //given
